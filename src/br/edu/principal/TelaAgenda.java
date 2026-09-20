f454e4536669065ba7dda0221f58237886b2de9e
Bryan9895 <bryan.william10@aluno.ifce.edu.br>
2026-09-20 15:41:28 -0300

    1.4.0 - Adicionando classe telaAgenda

---
+++ src/br/edu/principal/TelaAgenda.java
@@ -0,0 +1,19 @@
+package br.edu.principal;
+
+import javax.swing.JFrame;
+
+public class TelaAgenda extends JFrame {
+
+    public TelaAgenda() {
+
+        setTitle("Agenda de Contatos");
+
+        setSize(700, 500);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+}
